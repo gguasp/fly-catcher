e139ad1637400ed9580ed36dcf2a381a05a8927b
Guille <gguasp@cifpfbmoll.eu>
2021-10-24 22:12:59 +0200

    Name added and bugs fixed

--- fly.java
+++ fly.java
@@ -1,14 +1,16 @@
 
 public class fly(){
 
-  private weight;
-  private size;
-  private color;
+  private int weight;
+  private int size;
+  private string color;
+  private string name;
 
-  public fly(int size, int weight, string color){
+  public fly(int size, int weight, string color, string name){
       this.size = size;
       this.weight = weight;
       this.color = color;
+      this.name = name;
   }
 
 }
